01ea4951ed290e0cae24812e45717ad47203f083
Mubaris NK <hello@mubaris.com>
2020-04-02 17:08:58 +0530

    init

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import {ContainerApplication} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {ContainerApplication};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new ContainerApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
